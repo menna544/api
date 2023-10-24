5179034cd424b942e971ed8908c1ba69b126a1c3
menna544 <116039213+menna544@users.noreply.github.com>
2023-10-25 01:41:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ const getAllProducts = async () => {
     const res = await fetch('https://dummyjson.com/products')
     const data = await res.json()
     renderproduct(data.products)
+    console.log(data)
 }
 getAllProducts();
 function renderproduct(data){
     data.forEach(producte => {
         productcontainer.innerHTML +=
-`<div class="content">
+`
+<div class="content">
 <div class="product">
         <div class="image">
             <img src="${producte.thumbnail}">
         </div>
-        <h1 >"${data.title}"</h1>
-        <P>An apple mobile which is nothing like apple</P>
-        <div class="price"> "${producte.price}"<del></del></div>
+        <h1 >"${producte.title}"</h1>
+        <P>"${producte.description}"</P>
+        <div class="price"> ${producte.price}$<del></del></div>
         <div class="button">
             <button><i class="fa-solid fa-bag-shopping"></i>Add to cart </button>
             <button class="heart"><i class="fa-regular fa-heart"></i></button>
